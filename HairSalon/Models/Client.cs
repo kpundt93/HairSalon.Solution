42a5e0abda4e9c5f1ca47eb5f4dbc4ef7614fa36
Katie Pundt <kzpundt@gmail.com>
2021-12-24 09:39:55 -0800

    set up classes

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,13 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Specialty { get; set; }
+
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
