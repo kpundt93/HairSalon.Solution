51c2d3de79ec6b1f8ff9df8df5fdec679256c8fc
Katie Pundt <kzpundt@gmail.com>
2021-12-24 13:36:27 -0800

    add Index route and corresponding Clients/Index view

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+  }
+}
